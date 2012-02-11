e90920542e2f647d54027b4c6197affc1ebff02c
KTruong.Nguyen <ktruong.nguyen@gmail.com>
2012-02-11 02:28:36 +0000

    random_player is now fixed. It has no intelligence at all, just play random move
    Added new AILearning_Player.java, which is old random_player.java renamed to be the correct one.
    
    Everyone should get all files:
    1) interactive_Player.java
    2) KalahPieGameState.java
    3) MancalaGameState.java
    4) MancalaPlayer.java
    5) Node.java (disregard NewNode.java)
    6) random_Player.java
    7) RegressionLearning.java
    8) RegressionState.java
    9) TermGame.java

--- mancala/src/random_Player.java
+++ mancala/src/random_Player.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 /*
  * author: William Lam
  * author: Andrew Furusawa
  * 
  * class name: random_Player
- * description: 
+ * description: this player will just play random move, 
+ * there is no intelligence whatsoever
  */
 
 public class random_Player implements MancalaPlayer {
@@ -11,58 +14,37 @@ public class random_Player implements MancalaPlayer {
 	private int player;
 	private int opponent;
 	
-	RegressionLearning ai;
+	
 
 	public random_Player (int playerNum) {
 	  player = playerNum;
 	  opponent = 1 - player;
 	 
-	  // initialize ai and load theta values.
-	  ai = new RegressionLearning(playerNum,"data.txt");
-	 
-	  
 	}
 	
 	public int getMove(MancalaGameState gs) throws Exception {
 
-		ai.board = gs.copy();
-		int bestMove = -1;
-		Node evalNode = new Node(gs.copy(), 0);
-		bestMove = ai.findBestMove(evalNode, bestMove);
+		boolean inputOK = false;
+		int randomInt;
+		do {
+			Random r = new Random();
+			randomInt = r.nextInt(6);
+
+			inputOK = gs.validMove(randomInt);
+
+		} while (!inputOK);
+
+		return randomInt;
 		
-		if (bestMove != -1) {
-			RegressionState aState = new RegressionState(gs.copy(),player);
-			ai.updateHistory(aState);
-		}
-		 
-		return bestMove;
 	
 	}
 	
 	
 	/* This method will be used to learn from the game we just played. */
 	public Object postGameActions(MancalaGameState gs) {
-		ai.printThetas(ai.getWeight());
+		
 	    if (!gs.checkEndGame()) return null;
 	    
-//	    Make a copy to compute the final score
-	    MancalaGameState gsCopy = gs.copy();
-	    gsCopy.computeFinalScore();
-	    RegressionState aState = new RegressionState(gsCopy,player);
-		ai.updateHistory(aState);
-		ai.incGamesPlayed();
-		System.out.println(ai.getGamesPlayed());
-		
-	    if (gsCopy.getScore(player) > gsCopy.getScore(1-player))
-	        ai.setReward(1);
-	    else if (gsCopy.getScore(player) < gsCopy.getScore(1-player)) 
-	        ai.setReward(-1);
-	    else 
-	        ai.setReward(0);
-	    
-	    ai.gradientDescent((double)0.01);
-	    ai.printThetas(ai.getWeight());
-	    ai.saveThetas("data.txt");
 	    return null;
 	}
 
